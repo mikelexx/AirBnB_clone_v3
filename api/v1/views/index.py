e4932b4b96c49e43c378e57978947f69b3bbc2e4
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-24 22:04:58 +0300

    use classname strings

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,6 @@
 defines status route for api
 """
 from flask import jsonify
-from . import app_views
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
@@ -14,23 +13,21 @@ from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-@app_views.route('/status/', methods=['GET'])
 def api_status():
     """returns the status of API"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
-@app_views.route('/stats/', methods=['GET'])
 def models_stats():
     """ returns the status of objects"""
     models = {
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
-        "states": State,
-        "users": User
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
     }
     stats_dict = {}
     for model in models:
