20172051cb7a2dd2e8676a71deba32e0c507472d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-25 10:30:16 +0300

    use all the data fromm request to create a state object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -56,7 +56,7 @@ def create_state_obj():
         abort(400, "Not a JSON")
     if "name" not in data:
         abort(400, "Missing name")
-    state = State(name=json.get("name"))
+    state = State(**data)
     storage.new(state)
     storage.save()
     return jsonify(state.to_dict()), 201
