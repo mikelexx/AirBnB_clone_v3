e00d025fd92496ade15c4d89242784a15dcca4b8
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-26 08:21:50 +0300

    try using absolute imports

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,7 +3,7 @@
 defines api for accessing user objects
 """
 from flask import jsonify, abort, request
-from . import app_views
+from api.v1.views import app_views
 from models.user import User
 from models import storage
 
