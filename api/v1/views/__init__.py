b3c95cb005e33b74ef689ce03ca0b90dfed8535d
qlibane <qjirmo@gmail.com>
2024-05-27 11:42:56 -0700

    updated init.py file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-this file creates app view blueprint
-"""
+"""Init file for views module"""
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-app_views.strict_slashes = False
+
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -13,3 +13,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
