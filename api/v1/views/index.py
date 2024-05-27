7cad8a375d5b712e9c77f2e07e685f5c45299dd6
qlibane <qjirmo@gmail.com>
2024-05-27 11:09:02 -0700

    fixing the vi files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""
-defines status route for api
-"""
-from flask import jsonify
+""" Index """
 from api.v1.views import app_views
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-from models.user import User
-from models.state import State
+from flask import jsonify
 from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    """returns the status of API"""
-    return jsonify({"status": "OK"})
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def models_stats():
-    """ returns the status of objects"""
-    models = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
-    }
-    stats_dict = {}
-    for model in models:
-        stats_dict[model] = storage.count(models[model])
-    return jsonify(stats_dict)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
